342e3bf84993a5286ed86c9d4ebfd1d96443dfd1
PriyanshuSharma007 <selfcoder01@gmail.com>
2025-05-29 03:13:39 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: { fontFamily: {
+      mullish: ["Mullish", "sans-serif"],
+    },
+    colors:{
+      deepBlue: "#02042a", 
+      lightBlue300: "#4b94ed", 
+      lightBlue: "#2b84ea", 
+      greenLight: "#61cea6", 
+      lightBlue500: "#0b72e7", 
+      lightGray: "#e2e2e2", 
+      grayText: "#818597", 
+      grayBlue: "#344a6c", 
+      gray2: "#525a76",
+      deepBlueHead: "#162f56", 
+    },},
+  },
+  
+  plugins: [],
+}
+
